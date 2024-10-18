1720bdda4575e352bfa4835434c482cde6c99511
AR3ZzZz <163415664+AR3ZzZz@users.noreply.github.com>
2024-10-18 18:20:26 +0200

    Game2

--- Assets/Script/Enemigo.cs
+++ Assets/Script/Enemigo.cs
@@ -1,10 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Enemigo : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] Transform target;
+    [SerializeField] float minDistance;
+    float timer = 0;
+
+   
     void Start()
     {
         
@@ -13,6 +18,42 @@ public class Enemigo : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (EnRango())
+        {
+            if (timer < 0)
+            {
+                Debug.Log("Piu");
+                timer = Random.Range(3, 7);
+            }
+            else
+            {
+                timer -= Time.deltaTime;
+            }
+            
+
+        }
         
+    }
+
+    bool EnRango()
+    {
+        float distance = Vector3.Distance(transform.position, target.position);
+
+        if (distance < minDistance && target != null)
+        {
+            transform.LookAt(target);
+            bool dentroDeRango = true;
+            return dentroDeRango;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, minDistance);
     }
 }
